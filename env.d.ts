43904da505f105b11d24495f6c8e33dcc10e721f
Hosting <163719152+hosting-everyone@users.noreply.github.com>
2024-06-09 13:48:47 +0200

    Update env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -2,14 +2,14 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       DATABASE_URL: string;
-      BOT_TOKEN: MTI0OTMyMTY1MDEwNTc0OTU5NQ.GwGrgR.33pFSd4ewloPdkFF6m9meCu-g20GOtmi5sCcJI
+      BOT_TOKEN: string;
 
       /**
        * The url where the frontend is hosted
        *
        * ex: `https://my-bot.vercel.app`, default: `http://localhost:3000`
        */
-      WEB_URL?: https://crocoboco.vercel.app
+      WEB_URL?: string;
 
       PORT?: string;
     }
